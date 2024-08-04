db57d1c21485ccdd1a20617b0d93b7b1a8f33fdd
Andy <thoaitran93@gmail.com>
2024-08-04 11:49:11 +0700

    part 2 handle error

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,13 +1,21 @@
+'use client';
+
+import { useActionState } from 'react';
+import { useFormState } from 'react-dom';
 import {CustomerField} from "@/app/dashboard/definitions";
 import {CheckIcon, ClockIcon, CurrencyDollarIcon, UserCircleIcon} from "@heroicons/react/24/outline";
 import Link from "next/link";
 import {Button} from "@/app/ui/button";
-import {createInvoice} from "@/app/lib/actions";
+import {createInvoice, State} from "@/app/lib/actions";
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+    const initialState: State = { message: '', errors: {} };
+
+    const [state, formAction] = useFormState(createInvoice, initialState);
+
 
     return (
-        <form action={createInvoice}>
+        <form action={formAction}>
             <div className="rounded-md bg-gray-50 p-4 md:p-6">
                 {/* Customer Name */}
                 <div className="mb-4">
@@ -20,6 +28,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                             name="customerId"
                             className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                             defaultValue=""
+                            aria-describedby="customer-error"
                         >
                             <option value="" disabled>
                                 Select a customer
@@ -30,7 +39,16 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                                 </option>
                             ))}
                         </select>
-                        <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+                        <UserCircleIcon
+                            className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500"/>
+                    </div>
+                    <div id="customer-error" aria-live="polite" aria-atomic="true">
+                        {state.errors?.customerId &&
+                            state.errors.customerId.map((error: string) => (
+                                <p className="mt-2 text-sm text-red-500" key={error}>
+                                    {error}
+                                </p>
+                            ))}
                     </div>
                 </div>
 
@@ -45,12 +63,22 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                                 id="amount"
                                 name="amount"
                                 type="number"
+                                aria-describedby="amount-error"
                                 step="0.01"
                                 placeholder="Enter USD amount"
                                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                             />
-                            <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                            <CurrencyDollarIcon
+                                className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"/>
                         </div>
+                    </div>
+                    <div id="amount-error" aria-live="polite" aria-atomic="true">
+                        {state.errors?.amount &&
+                            state.errors.amount.map((error: string) => (
+                                <p className="mt-2 text-sm text-red-500" key={error}>
+                                    {error}
+                                </p>
+                            ))}
                     </div>
                 </div>
 
@@ -73,7 +101,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                                     htmlFor="pending"
                                     className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-gray-100 px-3 py-1.5 text-xs font-medium text-gray-600"
                                 >
-                                    Pending <ClockIcon className="h-4 w-4" />
+                                    Pending <ClockIcon className="h-4 w-4"/>
                                 </label>
                             </div>
                             <div className="flex items-center">
@@ -88,12 +116,25 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                                     htmlFor="paid"
                                     className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-green-500 px-3 py-1.5 text-xs font-medium text-white"
                                 >
-                                    Paid <CheckIcon className="h-4 w-4" />
+                                    Paid <CheckIcon className="h-4 w-4"/>
                                 </label>
                             </div>
                         </div>
                     </div>
+                    <div id="status-error" aria-live="polite" aria-atomic="true">
+                        {state.errors?.status &&
+                            state.errors.status.map((error: string) => (
+                                <p className="mt-2 text-sm text-red-500" key={error}>
+                                    {error}
+                                </p>
+                            ))}
+                    </div>
                 </fieldset>
+                <div aria-live="polite" aria-atomic="true">
+                    {state.message ? (
+                        <p className="mt-2 text-sm text-red-500">{state.message}</p>
+                    ) : null}
+                </div>
             </div>
             <div className="mt-6 flex justify-end gap-4">
                 <Link
